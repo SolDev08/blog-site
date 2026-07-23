1eea6d9f1a1ba0fc121c20da4f33bef9e1e535e7
yozweb3 <zhengwenyao886@gmail.com>
2026-07-23 22:16:15 +0800

    add the page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
-   <div>
-    <h1 className="text-center text-4xl font-semibold  text-black sm:text-5xl">Welcome to my bolg.</h1>
-   </div>
+    <div>
+      <h1 className="text-center text-4xl font-semibold  text-black sm:text-5xl">Welcome to my bolg.</h1>
+    </div>
   );
 }
- 
